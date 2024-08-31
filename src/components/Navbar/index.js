3e688d28e0d1be7b0604e02d062aaf977a48000a
TrateU <trateu@clemson.edu>
2024-08-31 09:53:49 -0400

    Fixed Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,30 +9,23 @@ import {
 } from "./navbarElements";
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(true);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
-
     return (
         <>
             <Nav>
-                <Bars onClick={toggleMenu} />
-                <NavMenu style={{ display: isOpen ? "flex" : "none" }}>
-                    <NavLink to="/" onClick={() => setIsOpen(false)}>
+                <NavMenu style={{ "flex" : "none" }}>
+                    <NavLink to="/">
                         Home
                     </NavLink>
-                    <NavLink to="/scores" activeStyle onClick={() => setIsOpen(false)}>
+                    <NavLink to="/scores" activeStyle>
                         Score Board
                     </NavLink>
-                    <NavLink to="/matchups" activeStyle onClick={() => setIsOpen(false)}>
+                    <NavLink to="/matchups" activeStyle>
                         Individual Matchups
                     </NavLink>
-                    <NavLink to="/standings" activeStyle onClick={() => setIsOpen(false)}>
+                    <NavLink to="/standings" activeStyle>
                         Standings
                     </NavLink>
-                    <NavLink to="/teams" activeStyle onClick={() => setIsOpen(false)}>
+                    <NavLink to="/teams" activeStyle>
                         Teams
                     </NavLink>
                 </NavMenu>

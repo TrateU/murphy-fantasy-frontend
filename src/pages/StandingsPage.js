54f20c97e9901d6ea026a0ce42e56996d7f540a9
TrateU <trateu@clemson.edu>
2024-10-15 10:26:35 -0400

    Fixed Rankings

--- src/pages/StandingsPage.js
+++ src/pages/StandingsPage.js
@@ -40,10 +40,10 @@ export default function StandingsPage() {
         mData['teams'].sort((a,b) => {
             if(b.wPercentage !== a.wPercentage){
                 return b.wPercentage - a.wPercentage
-            }else if (b.wDivPercentage !== a.wDivPercentage){
-                return b.wDivPercentage - a.wDivPercentage
-            }else{
+            }else if (b.TotalFor !== a.TotalFor){
                 return b.TotalFor - a.TotalFor
+            }else{
+                return b.wDivPercentage - a.wDivPercentage
             }
         })
         jData['teams'].sort((a,b) => {
